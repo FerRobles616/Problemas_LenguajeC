056f8f2628a2b0e5c74884ae79742d133915983d
FerRobles616 <fernandojrl808@gmail.com>
2026-04-07 16:20:28 -0300

    a

---
+++ Semana3/Problema1.c
@@ -0,0 +1,21 @@
+/*Escribe un programa en C que lea un número entero y determine si es positivo,
+negativo o cero. Mostrar el mensaje correspondiente.*/
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Ingrese un número entero: ");
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        printf("El número es positivo.\n");
+    } else if (numero < 0) {
+        printf("El número es negativo.\n");
+    } else {
+        printf("El número es cero.\n");
+    }
+
+    return 0;
+}
