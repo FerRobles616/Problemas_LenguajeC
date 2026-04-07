e9f6e47b2ead15e69bf0c4eaed3d8c1aeeeca66e
FerRobles616 <fernandojrl808@gmail.com>
2026-04-07 17:00:14 -0300

    a

--- Semana3/Problema1.c
+++ Semana3/Problema1.c
@@ -4,11 +4,9 @@ negativo o cero. Mostrar el mensaje correspondiente.*/
 #include <stdio.h>
 
 int main() {
-    int numero;
-
     printf("Ingrese un número entero: ");
+    int numero;
     scanf("%d", &numero);
-
     if (numero > 0) {
         printf("El número es positivo.\n");
     } else if (numero < 0) {
@@ -16,6 +14,5 @@ int main() {
     } else {
         printf("El número es cero.\n");
     }
-
     return 0;
 }
